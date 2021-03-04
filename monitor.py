d053eee598a5ca441d89ceab44f1da31b9d2a0b1
Tauan Marinho <tauamarinho@gmail.com>
2021-03-04 18:03:04 -0300

    Adding memory usage

--- monitor.py
+++ monitor.py
@@ -22,13 +22,27 @@ if args.stamp:
 
 i = 0
 cpu = []
+mem = []
 while i < int(args.width):
-  cpu.append(psutil.virtual_memory().percent)
+  mem.append(psutil.virtual_memory().percent)
+  cpu.append(psutil.cpu_percent())
   time.sleep(int(args.stamp))
   i += 1
 
 print(cpu)
+print(mem)
 
+plt.figure(figsize=(10, 4))
+
+plt.subplot(231)
 plt.plot(cpu, 'bo', cpu, 'k')
 plt.ylabel('CPU')
+plt.xlabel('Time')
+
+plt.subplot(233)
+plt.plot(mem, 'bo', mem, 'k')
+plt.ylabel('Memory')
+plt.xlabel('Time')
+
+plt.suptitle('Status Monitor')
 plt.show()
